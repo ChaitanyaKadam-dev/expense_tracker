7af0b6d2264bc7abb8c792895e7c9de6c6f0b0e5
ChaitanyaKadam-dev <chaitanyaskadam3@gmail.com>
2023-05-01 03:22:47 +0530

    final

--- lib/expenses_list.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return 
-  }
-}
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -17,4 +28,29 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
 }
